e4cc1ab11458f217e0e5dc48ad9db480e662766a
Prithvi Vishak <prithvivishak@gmail.com>
2022-10-22 19:54:16 +0530

    Put secrets in config.h. DO NOT PUSH.

--- software/combadge/config.h
+++ software/combadge/config.h
@@ -8,8 +8,8 @@
 #define BUF_LEN 256
 #define BUF_FULL_INTERVAL BUF_LEN * int(1000000.0 * (1.0 / float(44100)))
 
-#define WIFI_SSID "YourSSID"
-#define WIFI_PASSWORD "YourPassword"
+#define WIFI_SSID "MVWiFi"
+#define WIFI_PASSWORD "A12168A17BC1519251521A18E219ECC"
 
 #define BUDDY_IP IPAddress(192, 168, 142, 40)
 #define UDP_PORT 1592
