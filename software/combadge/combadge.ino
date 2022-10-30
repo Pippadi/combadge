073f5b5803bd61f31a17b53cf7bd82374cdd0931
Prithvi Vishak <prithvivishak@gmail.com>
2022-10-30 19:30:48 +0530

    Enable basic speaker functionality

--- software/combadge/combadge.ino
+++ software/combadge/combadge.ino
@@ -6,6 +6,7 @@
 AsyncUDP udp;
 
 int16_t outgoingBuf[BUF_LEN];
+int16_t incomingBuf[BUF_LEN];
 volatile bool shouldTransmit = false;
 
 hw_timer_t* timer = NULL;
@@ -15,6 +16,18 @@ void IRAM_ATTR flagOffTransmit() {
 	portENTER_CRITICAL_ISR(&timerMux);
     shouldTransmit = true;
 	portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+void onPacket(AsyncUDPPacket packet) {
+    size_t size = packet.read((uint8_t*) incomingBuf, BYTES_PER_SAMPLE*BUF_LEN);
+    Serial.println(size);
+    Serial.println(incomingBuf[0]);
+
+    size_t written = 0; // Bytes written over I2S
+    esp_err_t err = i2s_write(SPK_PORT, incomingBuf, BUF_LEN, &written, 1000);
+    if (err != ESP_OK) {
+        Serial.println(written / BYTES_PER_SAMPLE);
+    }
 }
 
 void setup() {
@@ -28,15 +41,22 @@ void setup() {
 	}
 	Serial.println("Connected to WiFi!");
 	Serial.println(WiFi.localIP());
-    uint8_t ff = 255;
-    udp.writeTo(&ff, 1, BUDDY_IP, UDP_PORT);
+    // uint8_t ff = 255;
+    // udp.writeTo(&ff, 1, BUDDY_IP, UDP_PORT);
 
     if (!setupMicI2s()) {
-        Serial.println("Failed initializing driver");
+        Serial.println("Failed initializing microphone");
+        while (true);
+    }
+    if (!setupSpkI2s()) {
+        Serial.println("Failed initializing speaker");
         while (true);
     }
 
-	timer = timerBegin(0, 80, true);
+    udp.onPacket(onPacket);
+    Serial.println(udp.listen(UDP_PORT));
+
+	timer = timerBegin(MIC_TIMER, 80, true);
 	timerAttachInterrupt(timer, &flagOffTransmit, true);
 	timerAlarmWrite(timer, BUF_FULL_INTERVAL, true);
     timerAlarmEnable(timer);
@@ -45,9 +65,9 @@ void setup() {
 void loop() {
     if (shouldTransmit) {
         size_t incomingBytes = 0;
-        esp_err_t err = i2s_read(MIC_PORT, outgoingBuf, BUF_LEN * 2, &incomingBytes, 1000);
+        esp_err_t err = i2s_read(MIC_PORT, outgoingBuf, BUF_LEN * BYTES_PER_SAMPLE, &incomingBytes, 1000);
         if (err == ESP_OK) {
-            Serial.println(outgoingBuf[1]);
+            //Serial.println(outgoingBuf[1]);
             udp.writeTo((uint8_t*) outgoingBuf, incomingBytes, BUDDY_IP, UDP_PORT);
         }
         shouldTransmit = false;
--- software/combadge/config.h
+++ software/combadge/config.h
@@ -1,12 +1,23 @@
+#include "driver/i2s.h"
+
 #define MIC_WS 25
 #define MIC_SD 33
 #define MIC_BCLK 32
 #define MIC_PORT I2S_NUM_0
 #define MIC_TIMER 0
 
-#define SAMPLE_RATE 44100
+#define SPK_WS 13
+#define SPK_SD 27
+#define SPK_BCLK 12
+#define SPK_PORT I2S_NUM_1
+
+#define SAMPLE_RATE 16000
 #define BUF_LEN 256
-#define BUF_FULL_INTERVAL BUF_LEN * int(1000000.0 * (1.0 / float(44100)))
+#define BUF_FULL_INTERVAL BUF_LEN * int(1000000.0 * (1.0 / float(SAMPLE_RATE)))
+
+// Changing this means changing data types in the rest of the code.
+// This is just to make byte count to sample count conversions look prettier.
+#define BYTES_PER_SAMPLE 2
 
 #define WIFI_SSID "MVWiFi"
 #define WIFI_PASSWORD "A12168A17BC1519251521A18E219ECC"
