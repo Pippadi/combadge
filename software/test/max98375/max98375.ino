0e55a3f124dd8900f9944bbd330a2e2aeb2a0697
Prithvi Vishak <prithvivishak@gmail.com>
2023-01-31 16:16:17 +0530

    Switch to ESP-IDF driver from Arduino abstraction for I2S

--- software/test/max98375/max98375.ino
+++ software/test/max98375/max98375.ino
@@ -1,36 +1,57 @@
-#include "I2S.h"
+#include <driver/i2s.h>
 
-#define BUF_LEN 2048
+#define BUF_LEN 512
 #define SAMPLE_RATE 44100
 #define BITS_PER_SAMPLE 32
 
-#define FREQ 100
+#define FREQ 440
 #define PI 3.14
 
-uint32_t buf[BUF_LEN];
+int32_t buf[BUF_LEN];
+i2s_port_t spkPort = I2S_NUM_1;
 
 void setup() {
     Serial.begin(115200);
-    I2S.begin(I2S_PHILIPS_MODE, SAMPLE_RATE, BITS_PER_SAMPLE);
-    I2S.setDataPin(27);
-    I2S.setFsPin(13);
-    I2S.setSckPin(12);
+
+    const i2s_config_t spkCfg = {
+        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        // .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 4,
+        .dma_buf_len = BUF_LEN,
+    };
+
+    i2s_pin_config_t spkPins = {
+        .bck_io_num = 12,
+        .ws_io_num = 13,
+        .data_out_num = 27,
+    };
+
+    i2s_driver_install(spkPort, &spkCfg, 0, NULL);
+    i2s_set_pin(spkPort, &spkPins);
 }
 
 void loop() {
     static int n = 0;
     for (int i=0; i<BUF_LEN; i++) {
-        uint32_t sample = f(n);
+        int32_t sample = f(n);
         buf[i] = sample;
         n++;
         // Serial.println(sample);
     }
-    I2S.write(buf, BUF_LEN);
+
+    size_t bytesWritten;
+    i2s_write(spkPort, (char*) buf, BUF_LEN*8, &bytesWritten, portMAX_DELAY);
+
     delayMicroseconds(1000000 * (double(BUF_LEN) / double(SAMPLE_RATE)));
 }
 
-uint32_t f(int n) {
+int32_t f(int n) {
     double sinVal = sin((2.0 * PI * double(n*FREQ)) / double(SAMPLE_RATE));
-    uint32_t res = pow(2, BITS_PER_SAMPLE);
-    return (res / 2) + uint32_t(double(res) * sinVal);
+    uint32_t res = (pow(2, BITS_PER_SAMPLE) / 2) - 1;
+    return uint32_t(double(res) * sinVal);
 }
