145f9da5111c44afe384ed8a5372d4e8dba2a0f2
Prithvi Vishak <prithvivishak@gmail.com>
2023-01-31 19:46:08 +0530

    Correctly report buffer size in bytes

--- software/test/max98375/max98375.ino
+++ software/test/max98375/max98375.ino
@@ -2,18 +2,22 @@
 
 #define BUF_LEN 1024
 #define SAMPLE_RATE 8000
-#define BITS_PER_SAMPLE 32
+#define BITS_PER_SAMPLE 16
+#define BITS_PER_BYTE 8
 
-#define FREQ 440
+#define FREQ 1000
 #define PI 3.14
 
 #define DATA_PIN 27
 #define BCLK_PIN 12
 #define WS_PIN 13
 
-typedef int32_t sample_t;
+typedef int16_t sample_t;
 
+// Uncomment such lines if using I2S_CHANNEL_FMT_RIGHT_LEFT below
+// sample_t buf[BUF_LEN*2];
 sample_t buf[BUF_LEN];
+
 i2s_port_t spkPort = I2S_NUM_1;
 
 void setup() {
@@ -27,7 +31,7 @@ void setup() {
         .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
         .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 4,
+        .dma_buf_count = 8,
         .dma_buf_len = BUF_LEN,
     };
 
@@ -37,26 +41,39 @@ void setup() {
         .data_out_num = DATA_PIN,
     };
 
-    i2s_driver_install(spkPort, &spkCfg, 0, NULL);
-    i2s_set_pin(spkPort, &spkPins);
+    esp_err_t err;
+    err = i2s_driver_install(spkPort, &spkCfg, 0, NULL);
+    if (err != ESP_OK) {
+        Serial.println("Error installing driver");
+        Serial.println(err);
+        while(true);
+    }
+    err = i2s_set_pin(spkPort, &spkPins);
+    if (err != ESP_OK) {
+        Serial.println("Error setting pins");
+        Serial.println(err);
+        while(true);
+    }
 }
 
 void loop() {
-    static int n = 0;
+    static uint64_t n = 0;
     for (int i=0; i<BUF_LEN; i++) {
         sample_t sample = f(n);
+
+        // buf[i*2] = sample;
+        // buf[(i*2)+1] = sample;
         buf[i] = sample;
+
         n++;
-        // Serial.println(sample);
     }
 
     size_t bytesWritten;
-    i2s_write(spkPort, (char*) buf, BUF_LEN*8, &bytesWritten, portMAX_DELAY);
-
-    delayMicroseconds(1000000 * (double(BUF_LEN) / double(SAMPLE_RATE)));
+    // i2s_write(spkPort, (char*) buf, BUF_LEN*2*BITS_PER_SAMPLE/BITS_PER_BYTE, &bytesWritten, portMAX_DELAY);
+    i2s_write(spkPort, (char*) buf, BUF_LEN*BITS_PER_SAMPLE/BITS_PER_BYTE, &bytesWritten, portMAX_DELAY);
 }
 
-sample_t f(int n) {
+sample_t f(uint64_t n) {
     double sinVal = sin((2.0 * PI * double(n*FREQ)) / double(SAMPLE_RATE));
     sample_t res = (pow(2, BITS_PER_SAMPLE) / 2) - 1;
     return sample_t(double(res) * sinVal);
