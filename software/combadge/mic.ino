233bf398dd8d78f67afff708798075a8c1bc16f0
Prithvi Vishak <prithvivishak@gmail.com>
2023-02-04 17:39:20 +0530

    Update microphone I2S config

--- software/combadge/mic.ino
+++ software/combadge/mic.ino
@@ -1,21 +1,22 @@
 bool setupMicI2s() {
-    i2s_config_t conf = {};
-    conf.mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX);
-    conf.sample_rate = SAMPLE_RATE;
-    conf.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
-    conf.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT;
-    conf.communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB);
-    conf.dma_buf_count = 8;
-    conf.dma_buf_len = BUF_LEN;
-    conf.use_apll = false;
-    conf.tx_desc_auto_clear = false;
-    conf.fixed_mclk = 0;
+    i2s_config_t conf = {
+        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = i2s_bits_per_sample_t(BITS_PER_SAMPLE),
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+        .dma_buf_count = 8,
+        .dma_buf_len = BUF_LEN,
+        .use_apll = false,
+        .tx_desc_auto_clear = false,
+        .fixed_mclk = 0,
+    };
 
-    i2s_pin_config_t pins = {};
-    pins.bck_io_num = MIC_BCLK;
-    pins.ws_io_num = MIC_WS;
-    pins.data_in_num = MIC_SD;
-    pins.data_out_num = -1;
+    i2s_pin_config_t pins = {
+        .bck_io_num = MIC_BCLK,
+        .ws_io_num = MIC_WS,
+        .data_in_num = MIC_SD,
+    };
 
     esp_err_t err = i2s_driver_install(MIC_PORT, &conf, 0, NULL);
     if (err != ESP_OK)
