1299b0b636c3578c2d49c334cf19251215187dca
Prithvi Vishak <prithvivishak@gmail.com>
2023-07-21 10:32:13 +0530

    Add start and stop packets

--- software/combadge/combadge.ino
+++ software/combadge/combadge.ino
@@ -8,19 +8,18 @@
 #include "sounds/TNGChirp1.h"
 #include "sounds/TNGChirp2.h"
 
-WiFiServer server(LISTEN_PORT);
-WiFiClient client;
-WiFiClient incomingConn;
+#define AUDIO_START 0x01
+#define AUDIO_STOP 0x02
+#define AUDIO_DATA 0x03
+
+WiFiClient conn;
 
 MAX98357 spk;
 INMP441 mic;
 
-volatile bool shouldTransmit = false;
 volatile bool tapped = false;
 
 TaskHandle_t streamFromMicHandle;
-
-sample_t incomingBuf[BUF_LEN];
 
 void IRAM_ATTR registerTap() {
     tapped = true;
@@ -32,15 +31,7 @@ void setup() {
     adc_power_off();
     Serial.begin(115200);
 
-    WiFi.setSleep(true);
-    WiFi.setAutoReconnect(true);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    while (!WiFi.isConnected()) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("Connected to WiFi!");
-    Serial.println(WiFi.localIP());
+    establishConnection();
 
     I2SCfg i2scfg = {
         .sampleRate = SAMPLE_RATE,
@@ -73,53 +64,55 @@ void setup() {
     xTaskCreate(streamFromMic, "StreamFromMic", 10240, NULL, 0, &streamFromMicHandle);
 
     touchAttachInterrupt(TOUCH_PIN, registerTap, TOUCH_THRESHOLD);
-
-    server.begin(LISTEN_PORT, 1);
 }
 
 void loop() {
     size_t bytesRecvd, bytesWritten;
+    static sample_t incomingBuf[BUF_LEN+1];
 
-    while (!incomingConn) {
-        incomingConn = server.available();
-    }
-
-    Serial.println(incomingConn.remoteIP());
-    spk.wake();
-    playSound(HailBeep, HailBeepSizeBytes);
-
-    while (incomingConn.connected()) {
-        if (incomingConn.available()) {
-            bytesRecvd = incomingConn.read((uint8_t*) incomingBuf, BYTES_PER_SAMPLE*BUF_LEN);
-            if (!spk.write((char*) incomingBuf, bytesRecvd, &bytesWritten)) {
-                Serial.println(bytesWritten / BYTES_PER_SAMPLE);
-            }      
+    if (conn.available()) {
+        bytesRecvd = conn.read((uint8_t*) incomingBuf, BYTES_PER_SAMPLE*(BUF_LEN + 1));
+        if (bytesRecvd > 0) {
+            switch (incomingBuf[0]) { // Packet type
+                case AUDIO_START:
+                    spk.wake();
+                    playSound(HailBeep, HailBeepSizeBytes);
+                    break;
+                case AUDIO_STOP:
+                    spk.sleep();
+                    break;
+                case AUDIO_DATA:
+                    if (!spk.asleep())
+                        spk.write((char*) &incomingBuf[1], bytesRecvd - sizeof(sample_t), &bytesWritten);
+                    break;
+            }
         }
     }
-
-    spk.sleep();
+    else if (!conn.connected()) {
+        establishConnection();
+    }
 }
 
 void streamFromMic(void*) {
-    static sample_t outgoingBuf[BUF_LEN];
+    static sample_t outgoingBuf[BUF_LEN + 1];
 
     while (true) {
-        while (!tapped) { vTaskDelay(10 / portTICK_PERIOD_MS); }
+        while (!tapped || !conn.connected()) { vTaskDelay(10 / portTICK_PERIOD_MS); }
         playSound(TNGChirp1, TNGChirp1SizeBytes);
         waitTillTouchReleased();
+        conn.write(AUDIO_START);
 
-        client.connect(BUDDY_IP, LISTEN_PORT);
-        while (!client.connected()) { vTaskDelay(10 / portTICK_PERIOD_MS); };
-        while (client.connected() && !tapped) {
+        while (conn.connected() && !tapped) {
             // Dividing interval by two so that the buffer doesn't fill up before we're ready to send it
             vTaskDelay(BUF_FULL_INTERVAL_ms / 2 / portTICK_PERIOD_MS);
             // Using outgoingBuf directly because sample_t is int16_t already
-            size_t samplesRead = mic.read(outgoingBuf, BUF_LEN);
+            size_t samplesRead = mic.read(&outgoingBuf[1], BUF_LEN);
+            outgoingBuf[0] = AUDIO_DATA;
             if (samplesRead) {
-                client.write((uint8_t*) outgoingBuf, samplesRead*BYTES_PER_SAMPLE);
+                conn.write((uint8_t*) outgoingBuf, (samplesRead+1)*BYTES_PER_SAMPLE);
             }
         }
-        client.stop();
+        conn.write(AUDIO_STOP);
         playSound(TNGChirp2, TNGChirp2SizeBytes);
         waitTillTouchReleased();
     }
@@ -128,9 +121,11 @@ void streamFromMic(void*) {
 void playSound(const sample_t* sound, const size_t soundSizeBytes) {
     size_t bytesWritten;
     bool spkAsleep = spk.asleep();
-    if (spkAsleep) { spk.wake(); }
+    if (spkAsleep)
+        spk.wake();
     spk.write((char*) sound, soundSizeBytes, &bytesWritten);
-    if (spkAsleep) { spk.sleep(); }
+    if (spkAsleep)
+        spk.sleep();
 }
 
 void waitTillTouchReleased() {
@@ -138,4 +133,21 @@ void waitTillTouchReleased() {
         tapped = false;
         vTaskDelay(100 / portTICK_PERIOD_MS); // Give interrupt 100ms to fire
     }
+}
+
+void establishConnection() {
+    WiFi.setSleep(true);
+    WiFi.setAutoReconnect(true);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (!WiFi.isConnected()) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("Connected to WiFi!");
+    Serial.println(WiFi.localIP());
+
+    conn.stop();
+    conn.connect(BUDDY_IP, LISTEN_PORT);
+    while (!conn.connected()) { vTaskDelay(10 / portTICK_PERIOD_MS); };
+    Serial.print("Connected to "); Serial.println(BUDDY_IP);
 }
