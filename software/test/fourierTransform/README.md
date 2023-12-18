b8ac6a59d8822d35f8d63349ecb52171ffc35787
Prithvi Vishak <prithvivishak@gmail.com>
2023-12-18 13:40:30 +0530

    Allow Fourier transform script to read from sound file

---
+++ software/test/fourierTransform/README.md
@@ -0,0 +1,10 @@
+# fourierTransform
+
+A quick script that performs a Fourier analysis on a given signal for a specified number of coefficients,
+and plots the reconstructed waveform.
+
+```sh
+python3 fourierTransform.py soundFile.raw
+```
+
+Modify constants in the script for fine-tuning.
--- software/test/fourierTransform/fourierTransform.py
+++ software/test/fourierTransform/fourierTransform.py
@@ -2,13 +2,18 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-COEFFICIENT_CNT = 32
+import sys
 
 sine_coefficients = np.array([])
 cosine_coefficients = np.array([])
 
-samples = np.sin(2*np.pi*np.arange(0, 1024) / 256)
+samples = np.array([])
+# samples = np.sin(2 * np.pi * np.arange(0, 1024) / 256)
+with open(sys.argv[1], "rb") as f:
+    samples = np.frombuffer(f.read(), dtype=np.int16)
+
+# 16 found to be a good number
+COEFFICIENT_CNT = int(len(samples) / 16)
 
 sample_cnt = len(samples)
 interval = np.arange(sample_cnt)
@@ -20,18 +25,27 @@ for n in range(1, COEFFICIENT_CNT + 1):
     sinewave = np.sin(n * np.pi * interval / sample_cnt)
     coswave = np.cos(n * np.pi * interval / sample_cnt)
 
-    sine_coefficients = np.append(sine_coefficients, np.sum(samples * sinewave) / sample_cnt)
-    cosine_coefficients = np.append(cosine_coefficients, np.sum(samples * coswave) / sample_cnt)
+    sine_coefficients = np.append(
+        sine_coefficients, np.sum(samples * sinewave) / sample_cnt
+    )
+    cosine_coefficients = np.append(
+        cosine_coefficients, np.sum(samples * coswave) / sample_cnt
+    )
 
-    reconstructed = reconstructed + (sinewave*sine_coefficients[n-1]) + (coswave*cosine_coefficients[n-1])
+    reconstructed = (
+        reconstructed
+        + (sinewave * sine_coefficients[n - 1])
+        + (coswave * cosine_coefficients[n - 1])
+    )
 
 fig, axes = plt.subplots(2, 2)
 axes[0, 0].set_title("Sine coefficients")
-axes[0, 0].bar(range(1, COEFFICIENT_CNT+1), sine_coefficients)
+axes[0, 0].bar(range(1, COEFFICIENT_CNT + 1), sine_coefficients)
 axes[0, 1].set_title("Cosine coefficients")
-axes[0, 1].bar(range(1, COEFFICIENT_CNT+1), cosine_coefficients)
+axes[0, 1].bar(range(1, COEFFICIENT_CNT + 1), cosine_coefficients)
 axes[1, 0].set_title("Original waveform")
 axes[1, 0].plot(interval, samples)
 axes[1, 1].set_title("Reconstructed waveform")
 axes[1, 1].plot(interval, reconstructed)
+plt.tight_layout()
 plt.show()
